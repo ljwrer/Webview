344be99f388a9fa9cfec739f3386001cf74fcbdf
ljwrer <ljwrer@sina.com>
2015-05-29 17:36:26 +0800

    a1

---
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.webviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Message;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=(WebView)findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        Message
+        webView.loadUrl("http://3g.163.com/x/");
+    }
+
+
+}
